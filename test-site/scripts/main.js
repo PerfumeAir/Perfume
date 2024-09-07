7483be65175254a8e29998e50be0163996f9849c
PerfumeAir <15109153848@163.com>
2024-09-07 11:17:46 +0800

    updata4

--- test-site/scripts/main.js
+++ test-site/scripts/main.js
@@ -1,26 +1,7 @@
-let myButton = document.querySelector("button");
-let myHeading = document.querySelector("h2");
+$(function() {
+  $(".menu>ul>li").hover(function() {
+      $(this.querySelector("ul")).slideToggle()
+  })
+})
 
-function setUserName() {
-  let myName = prompt("请输入你的名字。");
-  if (!myName) {
-    setUserName();
-  } else {
-    localStorage.setItem("name", myName);
-    myHeading.textContent = "Hello," + myName;
 
-  }
-}
-
-  if (!localStorage.getItem("name")) {
-    setUserName();
-  } else {
-    let storedName = localStorage.getItem("name");
-    myHeading.textContent = "Hello," + storedName;
-
-  }
-  
-myButton.onclick = function () {
-    setUserName();
-  };
-  
